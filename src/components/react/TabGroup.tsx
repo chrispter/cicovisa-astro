c7f11819887f3f62e6fdec09ac795aa1f1dc5044
Christian MSO <aranrisck@gmail.com>
2022-11-14 20:27:01 -0600

    0.1.4 React was added

---
+++ src/components/react/TabGroup.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useState } from 'react';
+import { services } from './services';
+
+export const TabGroup = () => {
+
+    const [active, setActive] = useState(services[0]);
+
+    return (
+        <div className='w-full py-20 px-5 md:px-20 bg-cico-yellow' id="servicios">
+           
+            <div className='flex flex-col shadow-2xl bg-white'>
+                <h3 className="text-center font-bold text-cico-blue text-4xl uppercase mt-10">Nuestros productos y servicios</h3>
+                <div className='flex flex-row pt-5 px-5'>
+                    {services.map((service) => (
+                        <div 
+                            key={ service.id.toString() }
+                            className={`py-5 px-5 cursor-pointer ${ active.id === service.id ? 'opacity-100 border-b-4 border-cico-orange text-cico-orange font-semibold' : 'opacity-60 text-black'}`}
+                            onClick={() => setActive(service)}
+                        >{
+                            service.title
+                        }</div>
+                    ))}
+                </div>
+                <div className='flex flex-col md:flex-row justify-around items-center py-20 px-10 border-t-2 border-cico-light-blue'>
+                    <div className='w-full md:w-3/5'>
+                        <ul className='list-disc list-inside'>
+                            { active.list.map((item) => (
+                                <li key={ item }>{ item }</li>
+                            )) }
+                        </ul>
+                    </div>
+                    <div className='w-full md:w-2/5 py-10 md:py-0 md:px-5'>image</div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/react/services.ts
@@ -0,0 +1,37 @@
+export const services = [
+    {
+        id: 1,
+        title: 'Material Eléctrico',
+        list: [
+            'CABLES',
+            'LUMINARIAS SUBURBANAS',
+            'LUMINARIAS LED',
+            'LAMPARAS DE EMERGENCIA',
+            'REGULADORES',
+            'FOCOS',
+            'CONECTORES',
+            'TRABAJANDO CON LAS MEJORES MARCAS DEL MERCADO',
+        ],
+    },{
+        id: 2,
+        title: 'Equipamiento deportivo',
+        list: [
+            'CABLES',
+            'LUMINARIAS SUBURBANAS',
+            'LUMINARIAS LED',
+            'LAMPARAS DE EMERGENCIA',
+            'REGULADORES',
+        ]
+    },{
+        id: 3,
+        title: 'Juguetes',
+        list: [
+            'LUMINARIAS LED',
+            'LAMPARAS DE EMERGENCIA',
+            'REGULADORES',
+            'FOCOS',
+            'CONECTORES',
+            'TRABAJANDO CON LAS MEJORES MARCAS DEL MERCADO',
+        ]
+    }
+]
