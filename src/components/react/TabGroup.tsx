fa79de8040eacdf634265ee4fe2bf3fb0325abc8
Christian MSO <aranrisck@gmail.com>
2022-11-17 19:38:04 -0600

    0.1.5

--- src/components/react/TabGroup.tsx
+++ src/components/react/TabGroup.tsx
@@ -10,26 +10,32 @@ export const TabGroup = () => {
            
             <div className='flex flex-col shadow-2xl bg-white'>
                 <h3 className="text-center font-bold text-cico-blue text-4xl uppercase mt-10">Nuestros productos y servicios</h3>
-                <div className='flex flex-row pt-5 px-5'>
-                    {services.map((service) => (
-                        <div 
-                            key={ service.id.toString() }
-                            className={`py-5 px-5 cursor-pointer ${ active.id === service.id ? 'opacity-100 border-b-4 border-cico-orange text-cico-orange font-semibold' : 'opacity-60 text-black'}`}
-                            onClick={() => setActive(service)}
-                        >{
-                            service.title
-                        }</div>
-                    ))}
-                </div>
-                <div className='flex flex-col md:flex-row justify-around items-center py-20 px-10 border-t-2 border-cico-light-blue'>
-                    <div className='w-full md:w-3/5'>
-                        <ul className='list-disc list-inside opacity-60'>
-                            { active.list.map((item) => (
-                                <li key={ item }>{ item }</li>
-                            )) }
-                        </ul>
+                <div className='flex flex-col md:flex-row py-5'>
+                    <div className='flex w-full md:w-2/6 flex-row md:flex-col pt-5 px-5 overflow-x-auto'>
+                        {services.map((service) => (
+                            <div 
+                                key={ service.id.toString() }
+                                className={`py-5 px-5 cursor-pointer ${ active.id === service.id ? 'opacity-100 border-b-4 border-cico-orange text-cico-orange font-semibold' : 'opacity-60 text-black'}`}
+                                onClick={() => setActive(service)}
+                            >{
+                                service.title
+                            }</div>
+                        ))}
                     </div>
-                    <div className='w-full md:w-2/5 py-10 md:py-0 md:px-5'>image</div>
+                    <div className='w-full md:w-4/6 flex flex-col md:flex-row justify-center items-start py-20 px-10 border-t-2 border-cico-light-blue md:border-t-0 md:border-l-2'>
+                        <div className='w-full md:w-3/5'>
+                            <ul className='list-disc list-inside opacity-60'>
+                                { active.list.map((item) => (
+                                    <li key={ item }>{ item }</li>
+                                )) }
+                            </ul>
+                        </div>
+                        <div className='w-full md:w-2/5 py-10 md:py-0 md:px-5'>
+                            <div className="w-full aspect-square ">
+                                <img className=" w-full h-full object-center object-cover pointer-events-none rounded-xl" src={ active.image } alt={ active.title } />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
